f9608aad9911d865a456c7ef6d1ba1c1cc2f9202
PondusEin <PondusEin@users.noreply.github.com>
2020-09-10 15:49:20 +0200

    Made TTT board

--- Erik Holst Compulsory 1 Tic Tac Toe/Source.cpp
+++ Erik Holst Compulsory 1 Tic Tac Toe/Source.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 #include <stdlib.h>
+#include <conio.h>
+#include <string>
+#include <Windows.h>
+
+
+
+void printBoard() 
+{
+	char board[5][16] =
+	{
+		{"---------------"},
+		{'|', board[0], '|', board[1], '|', board[2], '|' },
+		{'|', board[3], '|', board[4], '|', board[5], '|' },
+		{'|', board[6], '|', board[7], '|', board[8], '|' },
+		{"---------------"},
+	}
+}
+
 
 void game() {
 
