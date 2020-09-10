07aa7ac3214098675e332aff371829f7e2583162
PondusEin <PondusEin@users.noreply.github.com>
2020-09-10 14:45:11 +0200

    Title and by me

--- Erik Holst Compulsory 1 Tic Tac Toe/Source.cpp
+++ Erik Holst Compulsory 1 Tic Tac Toe/Source.cpp
@@ -7,4 +7,14 @@ void game() {
 
 int main() {
 
+	std::cout << "\t\t\t\t\t" << "----------------------------------" << std::endl;
+	std::cout << "\t\t\t\t\t" << "|                                |" << std::endl;
+	std::cout << "\t\t\t\t\t" << "|          TIC TAC TOE           |" << std::endl;
+	std::cout << "\t\t\t\t\t" << "|                                |" << std::endl;
+	std::cout << "\t\t\t\t\t" << "|          By Erik Holst         |" << std::endl;
+	std::cout << "\t\t\t\t\t" << "----------------------------------" << std::endl;
+
+
+
+	return 0;
 }
