cd4153d39ec91b04c2480ae81f0ff8bcc619f7bd
PondusEin <69846011+PondusEin@users.noreply.github.com>
2020-09-15 22:22:20 +0200

    Finished printboard. Included all librarys. Title finished.

--- Erik Holst Compulsory 1 Tic Tac Toe/Source.cpp
+++ Erik Holst Compulsory 1 Tic Tac Toe/Source.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include <stdlib.h>
-#include <conio.h>
-#include <string>
+#include <conio.h>		// For _getch();
+#include <string>		// For writing answers
 #include <Windows.h>
-#include <cstdlib> // for rand() and srand()
-#include <ctime> // for time
+#include <cstdlib>		// for rand() and srand()
+#include <ctime>		// for time
+#include <vector>		// for vectors
+#include <algorithm>
 
 
 
 void printBoard() 
 {
-	char board[5][16] =
-	{
-		{"---------------"},
-		{'|', board[0], '|', board[1], '|', board[2], '|' },
-		{'|', board[3], '|', board[4], '|', board[5], '|' },
-		{'|', board[6], '|', board[7], '|', board[8], '|' },
-		{"---------------"},
-	}
+	const int size = 10;
+	int myarray[size] = {};
+	std::vector <char> board = { '1','2','3','4','5','6','7','8','9' }; //ikke int.
+
+	  for (int i = 0; i < board.size(); i++)
+	  {
+		  if (i % 3 == 0)
+		  {
+			  std::cout << '\n' << "-------------";
+			  std::cout << "\n" << '|';  
+		  }
+		  std::cout << ' ' << board[i] << ' '; //ikke 0
+		  std::cout << "|";
+	  }
+	  std::cout << '\n' << "-------------";
 }
 
 
@@ -30,10 +39,13 @@ int main() {
 	std::cout << "\t\t\t\t\t" << "----------------------------------" << std::endl;
 	std::cout << "\t\t\t\t\t" << "|                                |" << std::endl;
 	std::cout << "\t\t\t\t\t" << "|          TIC TAC TOE           |" << std::endl;
-	std::cout << "\t\t\t\t\t" << "|                                |" << std::endl;
-	std::cout << "\t\t\t\t\t" << "|          By Erik Holst         |" << std::endl;
+	std::cout << "\t\t\t\t\t" << "|             v 1.0              |" << std::endl;
 	std::cout << "\t\t\t\t\t" << "----------------------------------" << std::endl;
 
+	std::cout << '\n' << "Welcome to a simplistic version of Tic Tac Toe.  " << std::endl;
+
+
+	printBoard();
 	//Main menu
 			//Add players
 				//Add 1 player vs AI
